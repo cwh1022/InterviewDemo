1576dd66ee43a30d6e5b3b54f532b317d72aeee4
connor.chen <cwh1022@qq.com>
2019-10-31 10:57:35 +0800

    DCL

---
+++ src/main/java/com/atguigu/Interview/study/thread/SingletonDemo.java
@@ -0,0 +1,38 @@
+package com.atguigu.Interview.study.thread;
+
+/**
+ * Created on 2019/10/31
+ *
+ * @author connor.chen
+ */
+public class SingletonDemo {
+    private static SingletonDemo instance = null;
+
+    private SingletonDemo() {
+        System.out.println(Thread.currentThread().getName() + "\t 我是构造方法SingletonDemo（）");
+    }
+
+    // DCL(double check lock 双端检索机制)
+    public static SingletonDemo getInstance(){
+        if (instance == null){
+            synchronized (SingletonDemo.class) {
+                if (instance == null) {
+                    instance = new SingletonDemo();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public static void main(String[] args) {
+        //单线程场景下没问题，多线程会多次调用构造器
+        /*System.out.println(SingletonDemo.getInstance() == SingletonDemo.getInstance());
+        System.out.println(SingletonDemo.getInstance() == SingletonDemo.getInstance());
+        System.out.println(SingletonDemo.getInstance() == SingletonDemo.getInstance());*/
+        for (int i = 0; i < 20; i++) {
+            new Thread(() -> {
+                SingletonDemo.getInstance();
+            }, String.valueOf(i)).start();
+        }
+    }
+}
